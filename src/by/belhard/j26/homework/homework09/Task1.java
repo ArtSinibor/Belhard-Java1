761b820fb6547ae3bb852814cd7e4df3b956cd10
ArtSinibor <artsini@yandex.ru>
2020-12-14 20:30:26 +0300

    HW09 complete

--- src/by/belhard/j26/homework/homework09/Task1.java
+++ src/by/belhard/j26/homework/homework09/Task1.java
@@ -6,17 +6,21 @@ public class Task1 {
     public static void main(String[] args) {
         HashSet<Integer> set = new HashSet<Integer>();
 
-        while (set.size()<10){
-            set.add(-100 + (int) (Math.random() * 200));
+        while (set.size() < 10) {
+            set.add(getRandomInt());
         }
 
         System.out.println("Set:" + set);
 
-        int border = -100 + (int) (Math.random() * 200);
+        int border = getRandomInt();
         System.out.println("Border: " + border);
 
         removeLessThenBorder(set, border);
         System.out.println("New set: " + set);
+    }
+
+    private static int getRandomInt() {
+        return -100 + (int) (Math.random() * 200);
     }
 
     private static void removeLessThenBorder(HashSet<Integer> set, int border) {
